1d4e8fee87c2a0895d91a051634d94a055b18903
Rohit-Patekar1 <rohit.patekar.936@gmail.com>
2021-09-26 12:56:55 +0530

    Place order

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,35 +7,33 @@ import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
 
-    useEffect(() => {
-        dispatch(listProducts())
-
-    }, [dispatch])
-
-
-
-    return (
-        <div>
-            <h1>LATEST PRODUCTS</h1>
-            {loading ? <Loader /> : error ? (
-                <Message variant='danger'>{error}</Message>
-            ) : (
-                <Row>
-                    {products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>)}
-
-        </div>
-    )
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  )
 }
 
 export default HomeScreen
